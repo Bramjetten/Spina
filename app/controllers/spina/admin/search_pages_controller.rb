881bba90a057da066a960872cf47cdbbf77d3d0b
Bram Jetten <mail@bramjetten.nl>
2022-04-26 09:08:54 +0200

    page part

--- app/controllers/spina/admin/search_pages_controller.rb
+++ app/controllers/spina/admin/search_pages_controller.rb
@@ -3,7 +3,7 @@ module Spina
     class SearchPagesController < AdminController
       
       def index
-        @pages = Spina::Page.sorted.joins(:translations).includes(:translations).where(resource_id: params[:resource_id]).where(spina_page_translations: {locale: I18n.locale}).where("spina_page_translations.title ILIKE ?", "%#{params[:query]}%").page(params[:page])
+        @pages = Spina::Page.sorted.joins(:translations).includes(:translations).where(resource_id: params[:resource_id].presence).where(spina_page_translations: {locale: I18n.locale}).where("spina_page_translations.title ILIKE ?", "%#{params[:query]}%").page(params[:page])
       end
       
     end
